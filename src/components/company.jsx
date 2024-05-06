e9769833542198a7ea7db340b5efa8c8fb62dfcc
Rishad01 <rishadhusainrizvi786@gmail.com>
2024-05-06 22:45:41 +0530

    changes

--- src/components/company.jsx
+++ src/components/company.jsx
@@ -1,45 +1,62 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import img2 from "../Data/img2.png";
-import img3 from "../Data/img3.png";
-import img4 from "../Data/img4.png";
-import styles from './company.module.css'
-function Company(){
-return (
-  <div className={`${styles.container1}`}>
-    <h1 className={`${styles.heading}`}>Company</h1>
-    <hr className={`${styles.line}`} />
-    <div className={`${styles.container2}`}>
-      <a href="#">
-        <img
-          src={img2}
-          alt=" boat"
-          width={150}
-          height={200}
-          className={`${styles.cimage}`}
-        />
-      </a>
-      <a href="#">
-        <img
-          src={img3}
-          alt="csk"
-          width={150}
-          height={200}
-          className={`${styles.cimage}`}
-        />
-      </a>
-      <a href="#">
-        <img
-          src={img4}
-          alt="studds"
-          width={150}
-          height={200}
-          className={`${styles.cimage}`}
-        />
-      </a>
+import styles from "./company.module.css";
+import ShowMoreButton from "./showMore";
+function Company() {
+  const Cdata = [
+    {
+      cname: "boat",
+      cimg: "img2.png",
+      cap: "Market Cap(in cr.): ₹ 12,334",
+      price: "Current Price: ₹ 400",
+    },
+    {
+      cname: "studds",
+      cimg: "img4.png",
+      cap: "Market Cap(in cr.): ₹ 11,334",
+      price: "Current Price: ₹ 200",
+    },
+    {
+      cname: "csk",
+      cimg: "img3.png",
+      cap: "Market Cap(in cr.): ₹ 102,334",
+      price: "Current Price: ₹ 1400",
+    },
+  ];
+  return (
+    <div className={`${styles.container1}`}>
+      <h1 className={`${styles.heading}`}>Company</h1>
+      <hr className={`${styles.line}`} />
+      <div className={`${styles.outercontainer}`}>
+          {Cdata.map((data) => (
+            <div className={`${styles.innercontainer}`}>
+              <div className={`${styles.imagecontainer}`}>
+                <img
+                  src={data.cimg}
+                  alt={data.cname}
+                  className={`${styles.image}`}
+                />
+              </div>
+              <div>
+                <p>
+                  <h6 className={`${styles.valuecontainer}`}>{data.cap}</h6>
+                  <h6 className={`${styles.valuecontainer}`}>{data.price}</h6>
+                </p>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  className={` ${styles.buy} btn btn-success`}
+                >
+                  Buy Now
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
+      <ShowMoreButton />
+      <hr />
     </div>
-    <button className={`${styles.explore}`}>Explore More</button>
-  </div>
-);
+  );
 }
 
 export default Company;
