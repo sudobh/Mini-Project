7710a92b0c57213bfc6731320863efe57759473d
Rishad01 <rishadhusainrizvi786@gmail.com>
2024-05-06 14:11:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import Login from "./pages/LoginPage";
 import Signup from "./pages/SignupPage";
 import Company from "./components/CompanyPage";
+import OurCompany from "./components/ourCompany";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 function App() {
@@ -11,7 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/company" element={<Company />} />
+        <Route path="/company/:cname/:ccap/:cprice/:cpe/:cimg" Component={<Company />} />
+        <Route path="/ShowMoreButton" element={<OurCompany />} />
       </Routes>
     </BrowserRouter>
   );
