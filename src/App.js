ec36f315f0a0a0bf999606900773b159610a673a
Rishad01 <rishadhusainrizvi786@gmail.com>
2024-05-06 12:08:54 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Home from "./pages/Home";
+import Login from "./pages/LoginPage";
+import Signup from "./pages/SignupPage";
+import Company from "./components/CompanyPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/company" element={<Company />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navibar from "../components/Navbar";
+import Foot from "../components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Heroo from "../components/Hero";
+import Company from "../components/company";
+import Aboutus from "../components/AboutUS";
+function Home()
+{
+    return(
+    <div style={{backgroundColor:"black"}}>
+        <Navibar />
+        <Heroo/>
+        <Company/>
+        <Aboutus/>
+        <Foot />
+    </div>);
+}
+
+export default Home;
